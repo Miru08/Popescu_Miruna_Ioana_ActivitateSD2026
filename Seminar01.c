81d12a9b8c295aec8c2c4e235ec21ac4566b1f12
miruna.ioana0807 <miruna.ioana0807@gmail.com>
2026-02-25 23:39:25 +0200

    Problema facuta la seminarul 1.  structura produs + fct de initializare

--- Seminar01.c
+++ Seminar01.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Produs
+{
+	int id;
+	int stoc;
+	char* denumire; //ocupa in memorie 4/8 octeti, depinde de arhitecura, la noi e de 64 => 8 octeti
+	float pret;
+	char categorie;
+};
+
+struct Produs initializare(int idDat, int stocDat, const char* denumireData, float pretDat, char categorieData)
+{
+	struct Produs p;
+	p.id = idDat;
+	p.stoc = stocDat;
+	//p.denumire=denumireData => shallow copy atribuie adresa
+	p.denumire = (char*)malloc(sizeof(char) * (strlen(denumireData) + 1));	//cast explicit, transformam din void* in char *, 
+	//se calculeaza exact dimens ocupata de ce o sa fie in denumireData inmultind dimens unui char cu cate char-uri sunt
+	//p.denumire retine adresa de inceput a blocului alocat in heap => acum putem copia ce am primit in denumireData la zona alocata pe heap
+	strcpy_s(p.denumire, strlen(denumireData) + 1, denumireData);
+	p.pret = pretDat;
+	p.categorie = categorieData;
+	return p;
+}
 
 int main()
 {
-	printf("Introduceti un nr intreg: \n");
+	struct Produs prod1;
 
-	int var = 0;
-	scanf_s("%d", &var);
-	printf("Var introdusa: %d", var);
+	prod1 = initializare(1, 20, "Telefon Smart", 2000, 'A'); // "" pune null la final, in timp ce '' nu pune, avand doar un caracter
 
-	//citire var reala
 	return 0;
 }
