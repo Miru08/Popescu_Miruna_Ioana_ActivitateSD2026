6ac319b35ad030c89984033a0a622e6374c54e51
Miru08 <miruna.ioana0807@gamil.com>
2026-03-11 19:52:17 +0200

    seminar 4

---
+++ seminar04.c
@@ -0,0 +1,144 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina
+{
+    int id;
+    int nrUsi;
+    float pret;
+    char *model;
+    char *numeSofer;
+    unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+// creare structura pentru un nod dintr-o lista simplu inlantuita
+
+struct Nod
+{
+    Masina info;
+    struct Nod *next;
+};
+
+typedef struct Nod Nod;
+
+Masina citireMasinaDinFisier(FILE *file)
+{
+    char buffer[100];
+    char sep[3] = ",\n";
+    fgets(buffer, 100, file);
+    char *aux;
+    Masina m1;
+    aux = strtok(buffer, sep);
+    m1.id = atoi(aux);
+    m1.nrUsi = atoi(strtok(NULL, sep));
+    m1.pret = atof(strtok(NULL, sep));
+    aux = strtok(NULL, sep);
+    m1.model = malloc(strlen(aux) + 1);
+    strcpy(m1.model, aux);
+
+    aux = strtok(NULL, sep);
+    m1.numeSofer = malloc(strlen(aux) + 1);
+    strcpy(m1.numeSofer, aux);
+
+    m1.serie = *strtok(NULL, sep);
+    return m1;
+}
+
+void afisareMasina(Masina masina)
+{
+    printf("Id: %d\n", masina.id);
+    printf("Nr. usi : %d\n", masina.nrUsi);
+    printf("Pret: %.2f\n", masina.pret);
+    printf("Model: %s\n", masina.model);
+    printf("Nume sofer: %s\n", masina.numeSofer);
+    printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareListaMasini(Nod *cap)
+{
+    while (cap)
+    {
+        afisareMasina(cap->info);
+        cap = cap->next;
+    }
+}
+
+void adaugaMasinaInLista(Nod **cap, Masina masinaNoua)
+{
+    Nod *temp = (Nod *)malloc(sizeof(Nod));
+    temp->info = masinaNoua;
+    temp->next = NULL;
+    if (*cap == NULL)
+    {
+        *cap = temp;
+    }
+    else
+    {
+        Nod *p = *cap;
+        while (p->next)
+        {
+            p = p->next;
+        }
+        p->next = temp;
+    }
+}
+
+void adaugaLaInceputInLista(/*lista de masini*/ Masina masinaNoua)
+{
+    // adauga la inceputul listei o noua masina pe care o primim ca parametru
+}
+
+void *citireListaMasiniDinFisier(const char *numeFisier)
+{
+    FILE *file = fopen(numeFisier, "r");
+    Nod*cap=NULL;
+    while(!feof(file))
+    {
+        adaugaMasinaInLista(&cap, citireMasinaDinFisier(file));
+    }
+    fclose(file);
+    return cap;
+}
+
+void dezalocareListaMasini(Nod **temp)
+{
+    while(*temp)
+    {
+        free((*temp)->info.numeSofer);
+        free((*temp)->info.model);
+        Nod *nou = *temp;
+        nou = (*temp)->next;
+        free(*temp);
+        *temp = nou;
+
+    }
+}
+
+float calculeazaPretMediu(/*lista de masini*/)
+{
+    // calculeaza pretul mediu al masinilor din lista.
+    return 0;
+}
+
+void stergeMasiniDinSeria(/*lista masini*/ char serieCautata)
+{
+    // sterge toate masinile din lista care au seria primita ca parametru.
+    // tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
+}
+
+float calculeazaPretulMasinilorUnuiSofer(/*lista masini*/ const char *numeSofer)
+{
+    // calculeaza pretul tuturor masinilor unui sofer.
+    return 0;
+}
+
+int main()
+{
+    Nod *cap = citireListaMasiniDinFisier("masini.txt");
+    afisareListaMasini(cap);
+    dezalocareListaMasini(&cap);
+    return 0;
+}
