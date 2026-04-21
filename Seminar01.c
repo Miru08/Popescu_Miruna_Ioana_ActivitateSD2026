2ccb33ba1c126b8b7f61f8e27acd3bd597b92520
miruna.ioana0807 <miruna.ioana0807@gmail.com>
2026-04-21 14:20:33 +0300

    terminat

--- Seminar01.c
+++ Seminar01.c
@@ -1,72 +1,72 @@
-﻿//#include <stdio.h>
-//#include <stdlib.h>
-//
-//struct Produs
-//{
-//	int id;
-//	int stoc;
-//	char* denumire; //ocupa in memorie 4/8 octeti, depinde de arhitecura, la noi e de 64 => 8 octeti
-//	float pret;
-//	char categorie;
-//};
-//
-//struct Produs initializare(int idDat, int stocDat, const char* denumireData, float pretDat, char categorieData)
-//{
-//	struct Produs p;
-//	p.id = idDat;
-//	p.stoc = stocDat;
-//	//p.denumire=denumireData => shallow copy atribuie adresa
-//	p.denumire = (char*)malloc(sizeof(char) * (strlen(denumireData) + 1));	//cast explicit, transformam dintr-un pointer la void* in char*, 
-//	//se calculeaza exact dimens ocupata de ce o sa fie in denumireData, inmultind dimens unui char cu cate char-uri sunt
-//	//p.denumire retine adresa de inceput a blocului alocat in heap => acum putem copia ce am primit in denumireData la zona alocata pe heap
-//	strcpy_s(p.denumire, strlen(denumireData) + 1, denumireData);
-//	p.pret = pretDat;
-//	p.categorie = categorieData;
-//	return p;
-//}
-//
-//void afisare(struct Produs sursa)
-//{
-//	printf("Produsul cu id-ul %d, are denumirea %s, stocul: %d, pret: %5.2f si categoria: %c.\n", sursa.id, sursa.denumire, sursa.stoc, sursa.pret, sursa.categorie);
-//	//pe noul cadru de stiva, se rezerva spațiu fizic pentru o noua variabila locala numita sursa (care este de tip struct Produs).
-//	//Se copiaza automat fiecare octet din variabila originala prod1 (aflata în main) în noua structura sursa (aflata in afisare).
-//}
-//
-//void modificaDenumire(struct Produs* sursa, char* denumireNoua)
-//{
-//	free((*sursa).denumire);
-//	sursa->denumire = (char*)malloc(sizeof(char) * (strlen(denumireNoua) + 1));
-//	strcpy_s(sursa->denumire, strlen(denumireNoua) + 1, denumireNoua);
-//}
-//
-//void dezalocareMemorie(struct Produs* sursa)
-//{
-//	if (sursa->denumire != NULL)
-//	{
-//		free(sursa->denumire);//elibereaza efectiv blocul de pe heap dar sursa.denumire inca retine adresa de inceput a blocului eliberat, deci e bine sa punem NULL dupa free
-//		sursa->denumire = NULL;// ca sa nu mai retina adresa blocului sters din heap
-//	}
-//}
-//
-//float calculeazaTotal(struct Produs sursa)
-//{
-//	return sursa.stoc * sursa.pret;
-//}
-//
-//int main()
-//{
-//	struct Produs prod1;
-//	prod1 = initializare(1, 20, "Telefon Smart", 2000, 'A'); // "" pune null la final, in timp ce '' nu pune, avand doar un caracter
-//
-//	afisare(prod1);
-//
-//	modificaDenumire(&prod1, "Tableta");
-//	afisare(prod1);
-//
-//	printf("Valoarea totala pentru %s este %5.2f RON.\n", prod1.denumire, calculeazaTotal(prod1));
-//
-//	dezalocareMemorie(&prod1);
-//	afisare(prod1);
-//
-//	return 0;
-//}
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct Produs
+{
+	int id;
+	int stoc;
+	char* denumire; //ocupa in memorie 4/8 octeti, depinde de arhitecura, la noi e de 64 => 8 octeti
+	float pret;
+	char categorie;
+};
+
+struct Produs initializare(int idDat, int stocDat, const char* denumireData, float pretDat, char categorieData)
+{
+	struct Produs p;
+	p.id = idDat;
+	p.stoc = stocDat;
+	//p.denumire=denumireData => shallow copy atribuie adresa
+	p.denumire = (char*)malloc(sizeof(char) * (strlen(denumireData) + 1));	//cast explicit, transformam dintr-un pointer la void* in char*, 
+	//se calculeaza exact dimens ocupata de ce o sa fie in denumireData, inmultind dimens unui char cu cate char-uri sunt
+	//p.denumire retine adresa de inceput a blocului alocat in heap => acum putem copia ce am primit in denumireData la zona alocata pe heap
+	strcpy_s(p.denumire, strlen(denumireData) + 1, denumireData);
+	p.pret = pretDat;
+	p.categorie = categorieData;
+	return p;
+}
+
+void afisare(struct Produs sursa)
+{
+	printf("Produsul cu id-ul %d, are denumirea %s, stocul: %d, pret: %5.2f si categoria: %c.\n", sursa.id, sursa.denumire, sursa.stoc, sursa.pret, sursa.categorie);
+	//pe noul cadru de stiva, se rezerva spațiu fizic pentru o noua variabila locala numita sursa (care este de tip struct Produs).
+	//Se copiaza automat fiecare octet din variabila originala prod1 (aflata în main) în noua structura sursa (aflata in afisare).
+}
+
+void modificaDenumire(struct Produs* sursa, char* denumireNoua)
+{
+	free((*sursa).denumire);
+	sursa->denumire = (char*)malloc(sizeof(char) * (strlen(denumireNoua) + 1));
+	strcpy_s(sursa->denumire, strlen(denumireNoua) + 1, denumireNoua);
+}
+
+void dezalocareMemorie(struct Produs* sursa)
+{
+	if (sursa->denumire != NULL)
+	{
+		free(sursa->denumire);//elibereaza efectiv blocul de pe heap dar sursa.denumire inca retine adresa de inceput a blocului eliberat, deci e bine sa punem NULL dupa free
+		sursa->denumire = NULL;// ca sa nu mai retina adresa blocului sters din heap
+	}
+}
+
+float calculeazaTotal(struct Produs sursa)
+{
+	return sursa.stoc * sursa.pret;
+}
+
+int main()
+{
+	struct Produs prod1;
+	prod1 = initializare(1, 20, "Telefon Smart", 2000, 'A'); // "" pune null la final, in timp ce '' nu pune, avand doar un caracter
+
+	afisare(prod1);
+
+	modificaDenumire(&prod1, "Tableta");
+	afisare(prod1);
+
+	printf("Valoarea totala pentru %s este %5.2f RON.\n", prod1.denumire, calculeazaTotal(prod1));
+
+	dezalocareMemorie(&prod1);
+	afisare(prod1);
+
+	return 0;
+}
